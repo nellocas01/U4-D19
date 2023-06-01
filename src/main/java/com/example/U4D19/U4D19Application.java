2046652e108be9632a9e95a6ac807a06aae75969
Nello Casolla <nellocasolla446@gmail.com>
2023-06-01 14:20:43 +0200

    assets

---
+++ src/main/java/com/example/U4D19/U4D19Application.java
@@ -0,0 +1,13 @@
+package com.example.U4D19;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U4D19Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U4D19Application.class, args);
+	}
+
+}
